07fe43339b2b12df900f82599cb4327a1537cc50
Kevin Grant <ConsciousK@yahoo.com>
2019-10-22 21:19:43 -0400

    Began work on stretch goal. Date Picker implimented. Still need to get selected date into the api call.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React, { useState, useEffect} from "react";
+import React from "react";
 import "./App.css";
-import axios from 'axios';
 import ImageCard from './components/ImageCard'
 
-function App() {
-  const [data, setData] = useState({});
 
-  useEffect(() => {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=jad07zoq0RscmTZa5rSyscaha1LUNUHLm3ztewE0')
-      .then(res => {
-        console.log(res.data)
-        setData(res.data)
-      });
-  }, []);
+
+function App() {
 
   return (
     <div className="App">
-          <ImageCard title={data.title}
-                     url = {data.url}
-                     explanation = {data.explanation}
-                     copyright={data.copyright}/>
+          <ImageCard />
     </div>
+
   );
 }
 
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "react-datepicker/dist/react-datepicker.css";
+import DateSelector from "./DateSelector";
 
+export default function ImageCard() {
 
-export default function ImageCard(props) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=jad07zoq0RscmTZa5rSyscaha1LUNUHLm3ztewE0&date=2012-03-14`)
+        .then(res => {
+          console.log(res.data)
+          setData(res.data)
+        });
+    }, []);
+  
     return (
         <div>
-            <h2>{props.title}</h2>
-            <img src={props.url} alt={props.title} />
-            <p>{props.explanation}</p>
-            <h3>{props.copyright}</h3>
+            <DateSelector />
+            <h2>{data.title}</h2>
+            <img src={data.url} alt={data.title} />
+            <p>{data.explanation}</p>
+            <h3>{data.copyright}</h3>
         </div>
     )
 }

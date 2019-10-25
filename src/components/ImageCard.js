62e641deb13d36520a1e3634895c150a27602112
Kevin Grant <ConsciousK@yahoo.com>
2019-10-24 20:53:55 -0400

    Completed MVP and Styling.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,28 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import "react-datepicker/dist/react-datepicker.css";
-import DateSelector from "./DateSelector";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+
 
 export default function ImageCard() {
+  const [data, setData] = useState({});
 
-    const [data, setData] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=jad07zoq0RscmTZa5rSyscaha1LUNUHLm3ztewE0&`
+      )
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);
+      });
+  }, []);
 
-    useEffect(() => {
-      axios.get(`https://api.nasa.gov/planetary/apod?api_key=jad07zoq0RscmTZa5rSyscaha1LUNUHLm3ztewE0&date=2012-03-14`)
-        .then(res => {
-          console.log(res.data)
-          setData(res.data)
-        });
-    }, []);
-  
-    return (
-        <div>
-            <DateSelector />
-            <h2>{data.title}</h2>
-            <img src={data.url} alt={data.title} />
-            <p>{data.explanation}</p>
-            <h3>{data.copyright}</h3>
-        </div>
-    )
+  return (
+    <Card
+      style={{
+        margin: "0 25%",
+        border: "3px solid black"
+      }}
+    >
+      <Card.Title>{data.title}</Card.Title>
+      <Card.Subtitle style={{ marginBottom: ".75rem" }}>
+        {data.copyright}
+      </Card.Subtitle>
+      <Card.Img
+        src={data.url}
+        alt={data.title}
+        style={{
+          border: "1px solid black"
+        }}
+      />
+      <Card.Body>
+        <Card.Text
+          style={{
+            padding: "10px",
+            border: "1px solid black",
+            margin: "5px"
+          }}
+        >
+          {data.explanation}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
-
